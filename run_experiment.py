28b68c6ef9e1dab1bb25d2714dc6a449e4c64949
isabelchaves <isabelpchaves@gmail.com>
2020-05-02 17:51:39 +0100

    feat: adding word2vec experiments

--- run_experiment.py
+++ run_experiment.py
@@ -1,18 +1,23 @@
 from typing import Dict
 
 import pandas as pd
+
 from configs.experiment_config import ExperimentConfig
 from data.processing.feature_engineering import FeatureEngineering
 from data.processing.textual_processing import PreProcessing
 from experiments.tf_idf_experiments import TfIfdExperiments
+from word_representations.word2vec import Word2VecExperiments
 
 
 class RunExperiment:
-    def __init__(self, data_path: str, language: Dict, vector_space: str, vector_method: str):
+    def __init__(self, data_path: str, language: Dict, vector_space: str, vector_method: str,
+                 word_vectors_strategy: str, click_graph_interaction_number: int):
         self.data_path = data_path
         self.language = language
         self.vector_space = vector_space
         self.vector_method = vector_method
+        self.word_vectors_strategy = word_vectors_strategy
+        self.click_graph_interaction_number = click_graph_interaction_number
 
     def run(self):
         data = pd.read_csv(self.data_path + 'train.csv', encoding='latin-1')
@@ -21,8 +26,22 @@ class RunExperiment:
         data['search_term_processed'] = data['search_term'].apply(lambda x: language_process.tokenizer(x))
         data['product_title_processed'] = data['product_title'].apply(lambda x: language_process.tokenizer(x))
 
-        TfIfdExperiments(vector_space=self.vector_space,
-                         vector_method=self.vector_method).run(data=data)
+        tfidf = TfIfdExperiments(vector_space=self.vector_space,
+                                 vector_method=self.vector_method)
+
+        # baseline_product_vs, baseline_query_vs = tfidf.run_baseline(data=data)
+
+        clickgraph_product_vs, clickgraph_query_vs = \
+            tfidf.run_with_click_graph(data=data, click_graph_interaction_number=self.click_graph_interaction_number)
+
+        word2vec = Word2VecExperiments(word_vectors_strategy=self.word_vectors_strategy,
+                                       vector_method=self.vector_method,
+                                       vector_space=self.vector_space)
+
+        # baseline_product_vs, baseline_query_vs = word2vec.run_baseline(data=data)
+
+        # clickgraph_product_vs, clickgraph_query_vs = \
+        #    word2vec.run_with_click_graph(data=data, click_graph_interaction_number=self.click_graph_interaction_number)
 
         print(data.head())
 
@@ -32,5 +51,6 @@ if __name__ == '__main__':
     RunExperiment(data_path=experiment_config.data_path,
                   language=experiment_config.language,
                   vector_space=experiment_config.vector_space,
-                  vector_method=experiment_config.vector_method).run()
-
+                  vector_method=experiment_config.vector_method,
+                  word_vectors_strategy=experiment_config.word_vectors_strategy,
+                  click_graph_interaction_number=experiment_config.click_graph_interaction_number).run()
---
+++ word_representations/word2vec.py
@@ -0,0 +1,125 @@
+import gensim.downloader as api
+import numpy as np
+import pandas as pd
+
+from click_graph.click_graph_model import ClickGraphModel
+from data.processing.build_vector_spaces import build_vector_space
+from evaluation.evaluation_method import EvaluationMethod
+
+
+class Word2VecExperiments:
+    def __init__(self, word_vectors_strategy: str, vector_method: str, vector_space: str):
+        self.word_vectors_strategy = word_vectors_strategy
+        self.vector_method = vector_method
+        self.vector_space = vector_space
+        self.model = api.load('word2vec-google-news-300')
+
+    def _calculate_word_vectors(self, word_vectors, list_of_words, strategy):
+        """
+        https://stackoverflow.com/questions/46889727/word2vec-what-is-best-add-concatenate-or-average-word-vectors
+        """
+        vectors = []
+
+        for word in list_of_words:
+            if len(word) > 0:
+                try:
+                    vector = word_vectors[word]
+                    vector.astype(np.float16)
+                    vectors.append(vector)
+
+                except KeyError:
+                    nans_vector = np.zeros(300, dtype=int) + np.nan
+                    vectors.append(nans_vector)
+            pass
+
+        if strategy == 'sum':
+            return np.nansum([vectors], axis=1).flatten()
+
+        elif strategy == 'average':
+            return np.nanmean([vectors], axis=1).flatten()
+
+        else:
+            # return np.array([vectors])
+            raise ValueError('strategy must be either \'sum\' or \'average\'')
+
+    def _prepare_data(self, data):
+
+        product_title_vectors = data['product_title_processed'].apply(
+            lambda x: self._calculate_word_vectors(word_vectors=self.model,
+                                                   list_of_words=x,
+                                                   strategy=self.word_vectors_strategy))
+        search_term_vectors = data['search_term_processed'].apply(
+            lambda x: self._calculate_word_vectors(word_vectors=self.model,
+                                                   list_of_words=x,
+                                                   strategy=self.word_vectors_strategy))
+
+        products = dict()
+        queries = dict()
+
+        for i, row in data.iterrows():
+            if row['product_uid'] not in products:
+                products[row['product_uid']] = product_title_vectors[i]
+
+            if row['search_term_processed'] not in queries:
+                queries[row['search_term_processed']] = search_term_vectors[i]
+
+        return products, queries
+
+    def run_baseline(self, data: pd.DataFrame):
+        print('###################################################################')
+        print('########################   BASELINE   #############################')
+        print('###################################################################')
+
+        products, queries = self._prepare_data(data=data)
+
+        # Building Products vector space
+        product_vs = build_vector_space(data=products, vector_method=self.vector_method, vector_space=self.vector_space)
+
+        # Building Query vector space
+        query_vs = build_vector_space(data=queries, vector_method=self.vector_method, vector_space=self.vector_space)
+
+        EvaluationMethod().run(data=data,
+                               data_to_evaluate=queries,
+                               vector_space_to_search=product_vs,
+                               evaluate_column='search_term_processed')
+
+        return product_vs, query_vs
+
+    def run_with_click_graph(self, data: pd.DataFrame, click_graph_interaction_number: int):
+
+        print('###################################################################')
+        print('########################   CLICK GRAPH   ##########################')
+        print('###################################################################')
+
+        products, queries = self._prepare_data(data=data)
+
+        click_graph = ClickGraphModel(dimensions=self.model.vector_size,
+                                      data=data)
+
+        queries, products = click_graph.run(products=products,
+                                            queries=queries,
+                                            iterations_nr=click_graph_interaction_number,
+                                            start='document')
+
+        # Building Products vector space
+        product_vs = build_vector_space(data=products, vector_method=self.vector_method, vector_space=self.vector_space)
+
+        # Building Query vector space
+        query_vs = build_vector_space(data=queries, vector_method=self.vector_method, vector_space=self.vector_space)
+
+        print('QUERIES ANALYSIS')
+
+        EvaluationMethod().run(data=data,
+                               data_to_evaluate=queries,
+                               vector_space_to_search=product_vs,
+                               evaluate_column='search_term_processed')
+
+        # print('PRODUCTS ANALYSIS')
+        #
+        # EvaluationMethod().run(data=data,
+        #                        data_to_evaluate=products,
+        #                        vector_space_to_search=query_vs,
+        #                        evaluate_column='product_title_processed')
+
+        return product_vs, query_vs
+
