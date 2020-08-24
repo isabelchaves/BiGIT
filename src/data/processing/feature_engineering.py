a2e3bd60e0893b9fede650948ccb27850e5ab0bd
isabelchaves <isabelpchaves@gmail.com>
2020-08-24 12:12:57 +0200

    chore: clean FeatureEngineering class

--- src/data/processing/feature_engineering.py
+++ src/data/processing/feature_engineering.py
@@ -1,3 +1,4 @@
+import random
 from random import randint
 
 import pandas as pd
@@ -8,23 +9,21 @@ from src.configs.variables_const import VariablesConsts
 
 class FeatureEngineering:
     def __init__(self):
-        self.max_clicks = 200
+        self.max_clicks = 100
         self.min_clicks = 10
 
     def _create_click_score(self, data):
-        relevance_threshold = data.relevance.max()
+        relevance_threshold = data[VariablesConsts.RELEVANCE].quantile(0.60)
 
-        # data['clicks'] = data['relevance'].apply(
-        #     lambda x: randint(self.min_clicks*2, self.max_clicks) * x if x >= relevance_threshold else randint(0, self.min_clicks) * x)
-
+        random.seed(42)
         data[VariablesConsts.CLICKS] = data[VariablesConsts.RELEVANCE].apply(
-            lambda x: randint(self.min_clicks, self.max_clicks) if x == relevance_threshold else randint(0, self.min_clicks))
+            lambda x: randint(self.min_clicks, self.max_clicks) if x > relevance_threshold else randint(0, self.min_clicks))
 
-        data[VariablesConsts.CLICK_SCORE] = MinMaxScaler(feature_range=(0, 1)).fit_transform(data[[VariablesConsts.RELEVANCE]])
+        data[VariablesConsts.CLICK_SCORE] = MinMaxScaler(feature_range=(0, 1)).fit_transform(data[[VariablesConsts.CLICKS]])
 
-        data[VariablesConsts.CLICK_SCORE] = data.apply(
-            lambda x: (1 / (self.max_clicks * x[VariablesConsts.CLICK_SCORE])) * x[
-                VariablesConsts.CLICKS] if x.click_score else 0, axis=1)
+        # data[VariablesConsts.CLICK_SCORE] = data.apply(
+        #     lambda x: (1 / (self.max_clicks * x[VariablesConsts.CLICK_SCORE])) * x[
+        #         VariablesConsts.CLICKS] if x.click_score else 0, axis=1)
 
         return data
 
